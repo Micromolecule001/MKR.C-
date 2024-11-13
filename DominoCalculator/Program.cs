0acbfa5c56323e8af874c71e9fe7f5459a0133b2
Micromolecule001 <dima.boyko.bd@gmail.com>
2024-11-13 09:52:12 +0200

    'task'

---
+++ DominoCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // INPUT
+        int N = int.Parse(File.ReadAllText("INPUT.TXT"));
+
+        // dotal dot amount 
+        long totalDiamonds = 0;
+        for (int i = 0; i <= N; i++)
+        {
+            for (int j = i; j <= N; j++)
+            {
+                totalDiamonds += i + j;
+            }
+        }
+
+        // OUTPUT
+        File.WriteAllText("OUTPUT.TXT", totalDiamonds.ToString());
+
+        Console.WriteLine($"Сумарна кількість точок: {totalDiamonds}");
+    }
+}
+
